896052eac70d9b6cef7b9b714c3a2b5975d03ec8
pedroguerreiro <30011021@students.ual.pt>
2025-05-27 09:49:37 +0100

    a

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TraSim_FILE C:/Users/Alexandre/Desktop/FACULDADE/3ano/2 semestre/Laboratorio de Projeto/TraSim/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/TraSim.exe)
-set(__QT_DEPLOY_TARGET_TraSim_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TraSim_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
